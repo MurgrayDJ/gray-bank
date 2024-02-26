10b95deea0ffa49c0a1d8bb4b3907f5e66e695ef
murgraydj <murgraydj@gmail.com>
2024-02-26 02:49:55 -0500

    Adds SpringInitializr generated files
    Generated for Java 21

---
+++ graybank/src/main/java/com/murgray/graybank/GraybankApplication.java
@@ -0,0 +1,13 @@
+package com.murgray.graybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraybankApplication.class, args);
+	}
+
+}
---
+++ graybank/src/test/java/com/murgray/graybank/GraybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murgray.graybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
