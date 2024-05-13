62eb27c78c64ebdf75b51085529792ec36ffca6f
murgraydj <murgraydj@gmail.com>
2024-05-13 15:18:18 -0400

    Rebuild project for Java 17

--- gray-bank/src/main/java/com/murgray/graybank/GrayBankApplication.java
+++ gray-bank/src/main/java/com/murgray/graybank/GrayBankApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraybankApplication {
+public class GrayBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraybankApplication.class, args);
+		SpringApplication.run(GrayBankApplication.class, args);
 	}
 
 }
